5fbe727bacd9fc4789f1aadcf61b469e6d97db0a
1makarov <38854356+1makarov@users.noreply.github.com>
2022-03-22 16:51:02 +0300

    init

---
+++ README.md
@@ -0,0 +1,5 @@
+```shell
+apt update -y && apt install git -y
+
+git clone https://github.com/1makarov/aptos-installer
+```
---
+++ install.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+if [[ $EUID -ne 0 ]]; then
+           echo "This script must be run as root."
+              exit 1
+fi
+
+# Start.
+echo "Aptos Fullnode Installer - GitHub @1makarov"
+
+# Update and Install
+apt update
+apt install curl docker.io ca-certificates gnupg lsb-release wget -y
+
+# Install docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/bin/docker-compose
+chmod +x /usr/bin/docker-compose
+
+# Downloads configs...
+mkdir /home/aptos-full-node
+
+wget -O /home/aptos-full-node/docker-compose.yaml https://raw.githubusercontent.com/aptos-labs/aptos-core/main/docker/compose/public_full_node/docker-compose.yaml
+wget -O /home/aptos-full-node/public_full_node.yaml https://raw.githubusercontent.com/aptos-labs/aptos-core/main/docker/compose/public_full_node/public_full_node.yaml
+wget -O /home/aptos-full-node/genesis.blob https://devnet.aptoslabs.com/genesis.blob
+wget -O /home/aptos-full-node/waypoint.txt https://devnet.aptoslabs.com/waypoint.txt
+
+# Running docker-compose
+docker-compose -f /home/aptos-full-node/docker-compose.yaml up -d
